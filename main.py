c2a32b72f872c38013c947760801bd3d7a028ff4
Mohammad Javad <mrakhshani88@gmail.com>
2023-05-26 21:51:55 +0330

    Main file updated.

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import modules.translator.constant as constant
 
 input('Enter language: ')
 input("")
+input("")
 print(sl.get_selected_laguage())
 
 if len(sl.get_selected_laguage())!=0:
-    target_lang=input('Enter the target language')
+    target_lang=input('Enter the target language: ')
     user_choice= input('Your text: [(S) for speech or (T) for type): ').lower()
     if user_choice.lower() == 't':
         translator = Translator.Translator()
@@ -17,7 +18,7 @@ if len(sl.get_selected_laguage())!=0:
             text=input('Enter your text(Enter Q to quit): ')
             if text=='q':
                 break
-            print(translator.translate(text,constant.LANGUAGES[target_lang], constant.LANGUAGES[sl.get_language_list]))
+            print(translator.translate(text, constant.LANGUAGES[target_lang], constant.LANGUAGES[sl.get_selected_laguage()]))
     elif user_choice.lower() =='s':
         translator = Translator.Translator()
         rec=s.recognizer()
